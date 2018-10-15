fc206eb7cde0c4409a08e625c8764cbcf8e49811
jmadson <jmadson@rediker.com>
2018-10-15 18:52:11 -0400

    IO-923: Update QueryBuilder

--- src/Query/QueryBuilder.ts
+++ src/Query/QueryBuilder.ts
@@ -1,5 +1,7 @@
-import { merge } from "lodash";
-import { IModelAPIData } from "../Model";
+import * as hash from "object-hash";
+import { isEmpty, merge } from "lodash";
+
+import { getDataService } from "../Services";
 
 export type SortDirection = "asc" | "desc";
 
@@ -7,24 +9,39 @@ export interface ISort {
   [key: string]: SortDirection;
 }
 
-export type FilterValue = string | number | boolean | ISort;
+export type FilterValue = string | number | boolean;
 
 export interface IQueryBuilder {
+  /** Add the given sorting param to the query. Default SortDirection is "asc". */
   sort: (key: string, direction?: SortDirection) => IQueryBuilder;
+
+  /** Remove the given sorting param from the query. */
   removeSort: (key: string) => IQueryBuilder;
+
+  /** Add the given filtering criteria to the query. */
   filter: (key: string, value: FilterValue | FilterValue[]) => IQueryBuilder;
+
+  /** Remove the given filtering criteria from the query. */
   removeFilter: (key: string) => IQueryBuilder;
+
+  /** Set which page number to use in the query. */
   page: (pageNumber: number) => IQueryBuilder;
+
+  /** Set the query's page size. */
   pageSize: (pageSize: number) => IQueryBuilder;
+
+  /** Get a unique hashcode specific to the current queryParams */
   getHashCode: () => string;
+
+  /** Trigger the query by dispatching an action to the Redux data service. */
   invoke: () => void;
 }
 
-export interface IQueryParams<T extends IModelAPIData> {
-  sort?: ISort[];
+export interface IQueryParams {
+  sort?: ISort;
   page?: number;
   pageSize?: number;
-  filters?: {[P in keyof T]?: T[P]};
+  [key: string]: FilterValue | FilterValue[] | ISort;
 }
 
 /**
@@ -35,7 +52,7 @@ export interface IQueryParams<T extends IModelAPIData> {
  * ```
  * const queryBuilder = new QueryBuilder("student");
  *
- * student
+ * queryBuilder
  *   .filter("firstName", "Bob")
  *   .filter("state", "MA")
  *   .sort("lastName")
@@ -48,28 +65,31 @@ export interface IQueryParams<T extends IModelAPIData> {
  * Calling `invoke` will dispatch the query to the associated Redux data service, which will use a
  * QueryMapper to transform the query to the correct format for its data source.
  *
- * For example, if using a RestQueryMapper, it would return a string as query params in the following format:
+ * For example, if using a UrlQueryMapper, it would return a string as query params in the following format:
  * ```
  * ?firstName=Bob&state=MA&page=2&pageSize=100&sort=lastName,age:desc
  * ```
  *
- * Extend the QueryMapper as needed for your data source / API.
+ * Extend the QueryMapper as needed for your data source / API to change how the query built from
+ * QueryBuilder is transformed.
+ *
+ * Note that QueryBuilder is immutable. Each of the builder methods will return a new QueryBuilder instance.
  */
-export class QueryBuilder<T extends IModelAPIData> implements IQueryBuilder {
+export class QueryBuilder implements IQueryBuilder {
   public readonly serviceName: string;
-  public readonly queryParams: IQueryParams<T>;
+  public readonly queryParams: IQueryParams;
+  private hashCode: string;
 
-  public constructor(serviceName: string, queryParams: IQueryParams<T> = {}) {
+  public constructor(serviceName: string, queryParams: IQueryParams = {}) {
     this.serviceName = serviceName;
     this.queryParams = queryParams;
   }
 
   public filter(key: string, value: (FilterValue | FilterValue[])): IQueryBuilder {
-    const queryParams = merge({}, this.queryParams, {
-      filters: {
-        [key]: value,
-      },
-    });
+    const queryParams = {
+      ...this.queryParams,
+      key: value,
+    };
 
     return new QueryBuilder(this.serviceName, queryParams);
   }
@@ -83,13 +103,63 @@ export class QueryBuilder<T extends IModelAPIData> implements IQueryBuilder {
     return new QueryBuilder(this.serviceName, queryParams);
   }
 
-  public page: (pageNumber: number) => IQueryBuilder;
-  public pageSize: (pageSize: number) => IQueryBuilder;
-  public removeFilter: (key: string) => IQueryBuilder;
-  public removeSort: (key: string) => IQueryBuilder;
-  public sort: (key: string, direction?: SortDirection) => IQueryBuilder;
+  public pageSize(pageSize: number): IQueryBuilder {
+    const queryParams = {
+      ...this.queryParams,
+      pageSize,
+    };
 
-  public invoke: () => void;
-  public getHashCode: () => string;
+    return new QueryBuilder(this.serviceName, queryParams);
+  }
+
+  public sort(key: string, direction: SortDirection = "asc"): IQueryBuilder {
+    const queryParams = merge({}, this.queryParams, {
+      sort: {
+        [key]: direction,
+      },
+    });
+
+    return new QueryBuilder(this.serviceName, queryParams);
+  }
+
+  public removeFilter(key: string): IQueryBuilder {
+    const queryParams = {
+      ...this.queryParams,
+    };
+
+    delete queryParams[key];
+
+    return new QueryBuilder(this.serviceName, queryParams);
+  }
+
+  public removeSort(key: string): IQueryBuilder {
+    const queryParams = {
+      ...this.queryParams,
+    };
+
+    if ("sort" in queryParams) {
+      delete queryParams.sort[key];
+      if (isEmpty(queryParams.sort)) {
+        delete queryParams.sort;
+      }
+    }
+
+    return new QueryBuilder(this.serviceName, queryParams);
+  }
+
+  public invoke(): void {
+    getDataService(this.serviceName)
+      .actions
+      .fetchAll(this)
+      .invoke();
+  }
+
+  public getHashCode(): string {
+    if (!this.hashCode) {
+      this.hashCode = hash(this.queryParams);
+    }
+
+    return this.hashCode;
+  }
 
 }
