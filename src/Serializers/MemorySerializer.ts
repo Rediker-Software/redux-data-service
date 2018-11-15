514ccbf62d5c0c71062de20ca1a8ab2dde6a711f
jmadson <jmadson@rediker.com>
2018-11-15 17:28:38 -0500

    IO-1529: Update serializeQueryParams, fix tests

--- src/Serializers/MemorySerializer.ts
+++ src/Serializers/MemorySerializer.ts
@@ -1,4 +1,5 @@
 import { IModel, IModelData } from "../Model";
+import { IQueryParams } from "../Query";
 import { BaseSerializer } from "./BaseSerializer";
 
 /**
@@ -7,10 +8,14 @@ import { BaseSerializer } from "./BaseSerializer";
 export class MemorySerializer<T extends IModelData, R = T> extends BaseSerializer<Partial<R>, T, R> {
 
   public async serialize(model: IModel<T> | Partial<T>): Promise<Partial<R>> {
-    return await model as R; 
+    return await this.transform(model);
   }
 
   public async deserialize(data: Partial<R>): Promise<IModel<T>> {
-    return await data as any;
+    return await this.normalize(data);
+  }
+
+  public async serializeQueryParams(queryParams: IQueryParams): Promise<IQueryParams> {
+    return queryParams;
   }
 }
--- src/Serializers/RestSerializer.test.ts
+++ src/Serializers/RestSerializer.test.ts
@@ -76,26 +76,27 @@ describe("RestSerializer", () => {
   describe("serializeQueryParams", () => {
 
     it("convert the given IQueryParams object into a url-encoded string", async () => {
-      const fakeQueryParamData = {        
+      const fakeQueryParamData = {
         page: 1,
         pageSize: 2,
         grade: 4,
+        current: true,
+        name: "alice",
         values: ["bob", "jill"],
         sort: [
           {key: "firstName", direction: "asc" as SortDirection},
           {key: "lastName", direction: "desc" as SortDirection},
-        ],        
-      }
-      
-      const restSerializer = new RestSerializer(FakeModel);
-      const stubNormalize = stub(restSerializer, "normalize");
+        ],
+      };
 
+      const restSerializer = new RestSerializer(FakeModel);
       const urlEncodedString = await restSerializer.serializeQueryParams(fakeQueryParamData);
 
-      expect(urlEncodedString).to.equal("page=1&pageSize=2&grade=4&values=bob,jill&sort=firstName,lastName:desc");
+      expect(urlEncodedString).to.equal(
+        "page=1&pageSize=2&grade=4&current=true&name=alice&values=bob,jill&sort=firstName,lastName%3Adesc",
+      );
     });
 
-    
   });
 
 });
