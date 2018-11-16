9a112478553e23ff1a332639e138b26987aabe54
Jonathan Ziemnicki <JZiemnicki@Rediker.com>
2018-11-16 14:34:38 -0500

    IO-1529 Adding in faker name and string Interpolation - joy joy :)

--- src/Serializers/RestSerializer.test.ts
+++ src/Serializers/RestSerializer.test.ts
@@ -3,8 +3,9 @@ import { spy, stub } from "sinon";
 import { RestSerializer } from "./RestSerializer";
 import { FakeModel } from "../Model/Model.mock";
 
-import { lorem, random } from "faker";
+import { lorem, random, name } from "faker";
 import { SortDirection } from "../Query/QueryBuilder";
+import { diffTrimmedLines } from "diff";
 
 declare var intern;
 const { describe, it } = intern.getPlugin("interface.bdd");
@@ -76,27 +77,51 @@ describe("RestSerializer", () => {
   describe("serializeQueryParams", () => {
 
     it("convert the given IQueryParams object into a url-encoded string", async () => {
-      const fakeQueryParamData = {        
+
+      const fakeQueryParamData = {
         page: random.number().toString(),
         pageSize: random.number().toString(),
         grade: random.number().toString(),
         current: random.boolean().toString(),
-        values: [lorem.word().toString(), lorem.word().toString()],
+        values: [name.firstName(), name.firstName()],
         sort: [
-          {key: lorem.word().toString(), direction: "asc" as SortDirection},
-          {key: lorem.word().toString(), direction: "desc" as SortDirection},
-        ],        
+          {key: name.firstName(), direction: "asc" as SortDirection},
+          {key: name.firstName(), direction: "desc" as SortDirection},
+        ],
+      };
+
+      const restSerializer = new RestSerializer(FakeModel);
+      const urlEncodedString = await restSerializer.serializeQueryParams(fakeQueryParamData);
+     
+      // tslint:disable-next-line:max-line-length (with the backslash \ line continuation it seems to put in an extra space)
+      expect(urlEncodedString).to.equal(`page=${fakeQueryParamData.page}&pageSize=${fakeQueryParamData.pageSize}&grade=${fakeQueryParamData.grade}&current=${fakeQueryParamData.current}&values=${fakeQueryParamData.values[0]},${fakeQueryParamData.values[1]}&sort=${fakeQueryParamData.sort[0].key},${fakeQueryParamData.sort[1].key}${encodeURIComponent(":")}desc`);
+
+    });
+
+    it("convert the given IQueryParams object into a url-encoded string with special characters in names", async () => {
+      
+      const name1 = `${name.firstName()}%`;
+      const name2 =  `${name.firstName()},o`;
+
+      const fakeQueryParamData = {
+        page: random.number(),
+        pageSize: random.number(),
+        grade: random.number(),
+        current: random.boolean(),
+        values: [name.firstName(), name.firstName()],
+        sort: [
+          {key: `${name1}`, direction: "asc" as SortDirection},
+          {key: `${name2}`, direction: "desc" as SortDirection},
+        ],
       };
 
       const restSerializer = new RestSerializer(FakeModel);
       const urlEncodedString = await restSerializer.serializeQueryParams(fakeQueryParamData);
 
-      expect(urlEncodedString).to.equal("page=" + fakeQueryParamData.page + "&pageSize="
-       + fakeQueryParamData.pageSize + "&grade=" + fakeQueryParamData.grade + "&current=" + fakeQueryParamData.current +
-        "&values=" + fakeQueryParamData.values[0] + "," + fakeQueryParamData.values[1] +
-        "&sort=" + fakeQueryParamData.sort[0].key + "," + fakeQueryParamData.sort[1].key + encodeURIComponent(":") + "desc");
+      // tslint:disable-next-line:max-line-length (with the backslash \ line continuation it seems to put in an extra space)
+      expect(urlEncodedString).to.equal(`page=${fakeQueryParamData.page}&pageSize=${fakeQueryParamData.pageSize}&grade=${fakeQueryParamData.grade}&current=${fakeQueryParamData.current}&values=${fakeQueryParamData.values[0]},${fakeQueryParamData.values[1]}&sort=${encodeURIComponent(name1)},${encodeURIComponent(name2)}${encodeURIComponent(":")}desc`);
     });
-    
+
   });
 
 });
