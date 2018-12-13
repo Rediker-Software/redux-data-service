637ca9fe6b7446412bb3a4eac0255f4d4c58d8c1
Tom Peck <tpeck@rediker.com>
2018-12-13 17:12:34 -0500

    Fix imports, IO-1837

--- src/Services/DataService.mock.ts
+++ src/Services/DataService.mock.ts
@@ -1,6 +1,6 @@
 // tslint:disable:max-classes-per-file
 import { DataService } from "./DataService";
-import { IModelFactory } from "../Model";
+import { IModelFactory } from "../Model/IModel";
 import { FakeModel, IFakeModelData } from "../Model/Model.mock";
 
 export class FakeModelService extends DataService<IFakeModelData> {
