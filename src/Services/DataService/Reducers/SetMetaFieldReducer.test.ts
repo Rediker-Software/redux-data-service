eff801447809873553df4197b93b192425ccf8c1
Jonathan Scabich <jscabich@rediker.com>
2018-12-17 17:05:55 -0500

    IO-1533: SetMetaFieldReducer.test changes

--- src/Services/DataService/Reducers/SetMetaFieldReducer.test.ts
+++ src/Services/DataService/Reducers/SetMetaFieldReducer.test.ts
@@ -1,67 +1,108 @@
-// // tslint:disable:no-empty max-classes-per-file no-unused-expression
-// import { match, spy, stub } from "sinon";
-// import { ActionsObservable } from "redux-observable";
-// import { Observable } from "rxjs/Observable";
-// import "rxjs/add/observable/of";
-// import { Subject } from "rxjs/Subject";
-//
-// import { Map, Record } from "immutable";
-// import { createMockStore } from "redux-test-utils";
-// import hash from "object-hash";
-//
-// import { createMockServiceState } from "../../../TestUtils";
-// import { IModelMeta } from "../../../Model";
-// import { createMockFakeModel, createMockFakeModels, FakeModel, IFakeModelData } from "../../../Model/Model.mock";
-// import { MockAdapter } from "../../../Adapters/MockAdapter";
-// import { MockMapper } from "../../../Mapper/MockMapper";
-// import { MockSerializer } from "../../../Serializers";
-// import { configure } from "../../../Configure";
-//
-// import { DataService, IDataServiceState, IRequestCacheKey } from "./";
-// import { BaseService } from "../../BaseService";
-// import { registerService } from "../../ServiceProvider";
-//
-// declare var intern;
-// const { describe, it, beforeEach, afterEach } = intern.getPlugin("interface.bdd");
-// const { expect } = intern.getPlugin("chai");
-//
-// const noop = () => null;
-//
-// describe("DataService", () => {
-//   let fakeService;
-//   let mockAdapter;
-//   let store;
-//   let fakeModels;
-//   let state;
-//   const serviceName = "fakeModel";
-//   let mockMapper;
-//   let mockSerializer;
-//
-//   beforeEach(() => {
-//     configure({ modules: null });
-//     mockAdapter = new MockAdapter();
-//     mockMapper = new MockMapper();
-//     mockSerializer = new MockSerializer();
-//
-//     class FakeService extends DataService<IFakeModelData> {
-//       public name = serviceName;
-//       public ModelClass = FakeModel;
-//       protected _adapter = mockAdapter;
-//       protected _mapper = mockMapper;
-//       protected _serializer = mockSerializer;
-//     }
-//
-//     fakeService = new FakeService();
-//     registerService(fakeService);
-//
-//     fakeModels = createMockFakeModels();
-//
-//     state = createMockServiceState<IFakeModelData>(fakeService, [
-//       fakeService.actions.pushAll({ items: fakeModels }),
-//     ]);
-//     store = createMockStore(state);
-//   });
-//
-//   // TODO: Tests go here
-//
-// });
+// tslint:disable:no-empty max-classes-per-file no-unused-expression
+import "rxjs/add/observable/of";
+
+import { Map, Record } from "immutable";
+
+import { spy } from "sinon";
+
+import { fakeModelModule } from "../../../TestUtils/FakeModelModule";
+import { initializeTestServices, seedService } from "../../../TestUtils/Service";
+
+import { DataServiceStateRecord } from "../DataServiceStateRecord";
+import { setMetaFieldReducer, ISetMetaField } from "./SetMetaFieldReducer";
+import { FakeModel } from "../../../Model/Model.mock";
+import { IAction } from "../../IService";
+
+declare var intern;
+const { describe, it, beforeEach, afterEach } = intern.getPlugin("interface.bdd");
+const { expect } = intern.getPlugin("chai");
+
+describe("setMetaFieldReducer", () => {
+    let setRecordSpy;
+    let serviceName;
+    let model;
+
+    beforeEach(() => {
+      initializeTestServices(fakeModelModule);
+      model = seedService("fakeModel");
+      setRecordSpy = spy(Record.prototype, "set");
+      serviceName = "fakeModel";
+    });
+
+    afterEach(() => {
+      setRecordSpy.restore();
+    });
+
+    it("should set the meta field on the item with the new value", () => {
+      const state = DataServiceStateRecord({
+        items: Map({
+          [model.id]: model,
+        }),
+      });
+
+      const action: IAction<ISetMetaField<any>, any> = {
+        type: `${serviceName}/SET_META_FIELD`,
+        payload: {
+          id: model.id,
+          fieldName: "isLoading",
+          value: false,
+        },
+        meta: {},
+        invoke: spy(),
+      };
+      const newState = setMetaFieldReducer(state, action);
+      const updatedItem = newState
+        .get("items", null)
+        .get(model.id) as FakeModel;
+        
+      expect(updatedItem.isLoading).to.be.equal(action.payload.value);
+    });
+
+    it("should not set the metadata on the items on the record when id not found in items", () => {
+      const state = DataServiceStateRecord({
+        items: Map({
+          [model.id]: model,
+        }),
+      });
+      
+      const action: IAction<ISetMetaField<any>, any> = {
+        type: `${serviceName}/SET_META_FIELD`,
+        payload: {
+          id: "not likely to exist",
+          fieldName: "isLoading",
+          value: true,
+        },
+        meta: {},
+        invoke: spy(),
+      };
+
+      const newState = setMetaFieldReducer(state, action);
+
+      expect(setRecordSpy.calledWith("items")).to.be.false;
+    });
+
+    it("should update items with appropriate metadata when id found in items", () => {
+
+      const state = DataServiceStateRecord({
+        items: Map({
+          [model.id]: model,
+        }),
+      });
+
+      const action: IAction<ISetMetaField<any>, any> = {
+        type: `${serviceName}/SET_META_FIELD`,
+        payload: {
+          id: model.id,
+          fieldName: "isLoading",
+          value: true,
+        },
+        meta: {},
+        invoke: spy(),
+      };
+
+      const newState = setMetaFieldReducer(state, action);
+      const newModel = newState.items.get(model.id) as FakeModel;
+      expect(newModel.isLoading)
+        .to.equal(action.payload.value);
+    });
+});
